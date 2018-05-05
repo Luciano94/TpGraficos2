8a3955dcffa6ee86cf19e65ec78a27bdffcda1d1
Unknown <lucho.elp40@gmail.com>
2018-05-05 19:19:22 -0300

    V8.0
    -added sounds

--- GraficosTP2/GraficosTP2/main.cpp
+++ GraficosTP2/GraficosTP2/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
 #include <ctime>
 #define WINWIDTH 800;
 #define WINHEIGHT 600;
@@ -19,10 +20,21 @@ void closeEvent(sf::Event &event, sf::Window &window) {
 		window.close();
 }
 
-void bulletShot(float playerX, float playerY, sf::Sprite &bullet, float bulletSpeed, bool &bulletFlag) {
+void asteroidUpdate(sf::Sprite asteroids[], float winHeight, sf::Texture asteroidT, float &aSpeed, float elapsed) {
+	for (int i = 0; i < 10; i++) {
+		aSpeed = (rand() % 500);
+		if (asteroids[i].getPosition().y >(winHeight + asteroidT.getSize().y)) {
+			asteroids[i].setPosition(rand() % 600, 0);
+		}
+		else asteroids[i].move(0, aSpeed * elapsed);
+	}
+}
+
+void bulletShot(float playerX, float playerY, sf::Sprite &bullet, float bulletSpeed, bool &bulletFlag, sf::Sound &sound) {
 	if (!bulletFlag) {
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
 			bullet.setPosition(playerX, playerY);
+			sound.play();
 			bulletFlag = true;
 		}
 	}
@@ -35,41 +47,39 @@ void bulletShot(float playerX, float playerY, sf::Sprite &bullet, float bulletSp
 	}
 }
 
-void asteroidUpdate(sf::Sprite asteroids[], float winHeight, sf::Texture asteroidT, float &aSpeed, float elapsed) {
-	for (int i = 0; i < 10; i++) {
-		aSpeed = (rand() % 500);
-		if (asteroids[i].getPosition().y >(winHeight + asteroidT.getSize().y)) {
-			asteroids[i].setPosition(rand() % 600, 0);
-		}
-		else asteroids[i].move(0, aSpeed * elapsed);
-	}
-}
-
-void ColPlayerAsteroid(sf::Sprite &player, int &plifes, sf::Sprite asteroids[]) {
+void ColPlayerAsteroid(sf::Sprite &player, int &plifes, sf::Sprite asteroids[], sf::Sound &sound) {
 	sf::Texture asteroidT = *asteroids[0].getTexture();
-	sf::Texture playerT = *player.getTexture();
 	for (int i = 0; i < 10; i++) {
-		if ((player.getPosition().x <= asteroids[i].getPosition().x)
-			&& (player.getPosition().x + playerT.getSize().x >= asteroids[i].getPosition().x)
+		if (player.getPosition().x >= asteroids[i].getPosition().x
+			&& player.getPosition().x <= (asteroids[i].getPosition().x + asteroidT.getSize().x)
 			&& player.getPosition().y <= (asteroids[i].getPosition().y + asteroidT.getSize().y)) {
+			sound.play();
 			asteroids[i].setPosition(rand() % 600, 0);
 			plifes--;
 		}
 	}
 }
 
-void colBulletAsteroid(sf::Sprite &bullet, sf::Sprite asteroids[], int &score) {
+void colBulletAsteroid(sf::Sprite &bullet, sf::Sprite asteroids[], int &score, sf::Sound &sound) {
 	sf::Texture asteroidT = *asteroids[0].getTexture();
 	for (int i = 0; i < 10; i++) {
 		if (bullet.getPosition().x >= asteroids[i].getPosition().x
 			&& bullet.getPosition().x <= (asteroids[i].getPosition().x + asteroidT.getSize().x)
 			&& bullet.getPosition().y <= (asteroids[i].getPosition().y + asteroidT.getSize().y)) {
 
+			sound.play();
 			asteroids[i].setPosition(rand() % 600, 0);
 			bullet.setPosition(-8, -8);
 			score += 100;
 		}
 	}
+}
+
+void gameManager(int &plifes, int &score, bool &win, bool &lose) {
+	if (plifes == 0)
+		lose = true;
+	if (score == 10000)
+		win = true;
 }
 
 int main(){
@@ -130,9 +140,28 @@ int main(){
 	points.setFillColor(sf::Color::Red);
 	points.setStyle(sf::Text::Bold);
 	points.setPosition(600, 0);
+	/*GameManager*/
+	bool win = false;
+	bool lose = false;
 	/*Clock*/
 	sf::Clock clock;
 	float elapsed;
+	/*sounds*/
+	/*shot*/
+	sf::SoundBuffer shotbuffer;
+	shotbuffer.loadFromFile("sounds/playershot.wav");
+	sf::Sound shotsound;
+	shotsound.setBuffer(shotbuffer);
+	/*asteroids*/
+	sf::SoundBuffer astebuffer;
+	astebuffer.loadFromFile("sounds/enemyDeath.wav");
+	sf::Sound astesound;
+	astesound.setBuffer(astebuffer);
+	/*player*/
+	sf::SoundBuffer plabuffer;
+	plabuffer.loadFromFile("sounds/playerDeath.wav");
+	sf::Sound plasound;
+	plasound.setBuffer(plabuffer);
 	/*Windows Loop*/
 	while (window.isOpen()){
 		/*get elapsed time*/
@@ -144,24 +173,49 @@ int main(){
 		point = "Points: " + std::to_string(score);
 		points.setString(point);
 		/*Collisions*/
-		ColPlayerAsteroid(player, pLifes, asteroids);
-		colBulletAsteroid(bullet, asteroids, score);
+		ColPlayerAsteroid(player, pLifes, asteroids, plasound);
+		colBulletAsteroid(bullet, asteroids, score, astesound);
 		/*Game loop*/
 		movement(player, (speed * elapsed));
-		bulletShot(player.getPosition().x + 18, player.getPosition().y,bullet, bulletSpeed * elapsed, bulletFlag);
+		bulletShot(player.getPosition().x + 18, player.getPosition().y,bullet, bulletSpeed * elapsed, bulletFlag, shotsound);		
 		asteroidUpdate(asteroids, winHeight, asteroidT, aSpeed, elapsed);
+		gameManager(pLifes, score, win, lose);
 		/*Events Loop*/
 		sf::Event event;
 		while (window.pollEvent(event))
 			closeEvent(event, window);
 		/*Window update*/
 		window.clear();
-		for (int i = 0; i < 10; i++)
-			window.draw(asteroids[i]);
-		window.draw(player);
-		window.draw(bullet);
-		window.draw(lifes);
-		window.draw(points);
+		if (!win && !lose) {
+			for (int i = 0; i < 10; i++)
+				window.draw(asteroids[i]);
+			window.draw(player);
+			window.draw(bullet);
+			window.draw(lifes);
+			window.draw(points);
+		}
+		else {
+			if (win) {
+				lifes.setCharacterSize(68);
+				lifes.setPosition(250, 300);
+				lifes.setString("YOU WIN!!!");
+				window.clear();
+				window.draw(lifes);
+				/*Events Loop*/
+				while (window.pollEvent(event))
+					closeEvent(event, window);
+			}
+			else {
+				lifes.setCharacterSize(68);
+				lifes.setPosition(250, 300);
+				lifes.setString("YOU LOSE");
+				window.clear();
+				window.draw(lifes);				
+				/*Events Loop*/
+				while (window.pollEvent(event))
+					closeEvent(event, window);
+			}
+		}
 		window.display();
 	}
 	return 0;
